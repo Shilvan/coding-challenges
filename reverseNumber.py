3f95845a30439c75826c1b8f4cf89a8be61b0e50
Shilvan Tavares <52128472+Shilvan@users.noreply.github.com>
2022-02-07 12:08:29 +0000

    Create reverseNumber.py

---
+++ reverseNumber.py
@@ -0,0 +1,21 @@
+#Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+#Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+
+def reverse_number(n):
+    #Get n sign
+    sign = "+"
+    if (n < 0):
+        sign = "-"
+        
+    #split n with abs so the minus sign doesn't appear in the list
+    splittedN = list(str(abs(n)))
+    
+    #start with the sign
+    reversedN = sign
+    #reverse list and add to reversedN
+    for digit in splittedN[::-1]:
+        reversedN += digit
+        
+    #convert to integer
+    return int(reversedN)
